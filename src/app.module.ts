425b64f97be2d8160e7fa78d5ed72d3b2ab5bb48
Mikita Radzkou <mikita.radzkou@boweryvaluation.com>
2022-09-04 20:05:33 +0300

    Connect with PostgresSQL

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,23 +1,32 @@
-import { AppController } from './app.controller';
 import { Module } from "@nestjs/common";
-import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from "@nestjs/config";
 import { SequelizeModule } from '@nestjs/sequelize';
+import { UsersModule } from './users/users.module';
 
 
 @Module({
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
   imports: [
-    SequelizeModule.forRoot({
-      dialect: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: '12345',
-      database: 'nest-course',
-      models: [],
-      autoLoadModels: true
+    ConfigModule.forRoot({
+      envFilePath: `.${process.env.NODE_ENV}.env`
     }),
+    SequelizeModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: () => ({
+        dialect: 'postgres',
+        host: process.env.POSTGRES_HOST,
+        port: Number(process.env.POSTGRES_PORT),
+        username: process.env.POSTGRES_USER,
+        password: process.env.POSTGRES_PASSWORD,
+        database: process.env.POSTGRES_DB,
+        models: [],
+        autoLoadModels: true,
+        entities: ['dist/**/*.entity{.ts,.js}'],
+        synchronize: true
+      })
+    }),
+    UsersModule,
   ],
 })
 export class AppModule {}
