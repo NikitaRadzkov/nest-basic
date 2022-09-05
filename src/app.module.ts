021ed4efb092658da2647c1eca04ea241b3bdfb8
Mikita Radzkou <mikita.radzkou@boweryvaluation.com>
2022-09-05 19:44:54 +0300

    Added first requests

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,8 @@
-import { Module } from "@nestjs/common";
-import { ConfigModule, ConfigService } from "@nestjs/config";
-import { SequelizeModule } from '@nestjs/sequelize';
 import { UsersModule } from './users/users.module';
-
+import { Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
+import { SequelizeModule } from '@nestjs/sequelize';
+import { User } from "./users/users.model";
 
 @Module({
   controllers: [],
@@ -11,22 +11,17 @@ import { UsersModule } from './users/users.module';
     ConfigModule.forRoot({
       envFilePath: `.${process.env.NODE_ENV}.env`
     }),
-    SequelizeModule.forRootAsync({
-      imports: [ConfigModule],
-      useFactory: () => ({
+    SequelizeModule.forRoot({
         dialect: 'postgres',
         host: process.env.POSTGRES_HOST,
         port: Number(process.env.POSTGRES_PORT),
         username: process.env.POSTGRES_USER,
         password: process.env.POSTGRES_PASSWORD,
         database: process.env.POSTGRES_DB,
-        models: [],
+        models: [User],
         autoLoadModels: true,
-        entities: ['dist/**/*.entity{.ts,.js}'],
-        synchronize: true
-      })
     }),
-    UsersModule,
+    UsersModule
   ],
 })
 export class AppModule {}
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,14 @@
-import { Module } from '@nestjs/common';
+import { SequelizeModule } from '@nestjs/sequelize';
 import { UsersController } from './users.controller';
+import { Module } from "@nestjs/common";
 import { UsersService } from './users.service';
+import { User } from './users.model';
 
 @Module({
-  controllers: [UsersController],
-  providers: [UsersService]
+    controllers: [UsersController],
+    providers: [UsersService],
+    imports: [
+        SequelizeModule.forFeature([User])
+    ]
 })
 export class UsersModule {}
